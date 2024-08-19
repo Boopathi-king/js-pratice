87f328c3e428eb37ee6c3ffbdf3940cdfa3fd140
Boopathi-king <boopathiboopathi31@gmail.com>
2024-08-19 19:04:07 +0530

    first commit

---
+++ Assignment 2/Part I/script.js
@@ -0,0 +1,11 @@
+
+let a = 1;
+let b = 1;
+let c = ++a;
+let d = b++;
+
+console.log("value of a:", a);
+console.log( "Value of b:", b);
+console.log( "Value of c-->", c);
+console.log( "Value of d-->", d);
+
