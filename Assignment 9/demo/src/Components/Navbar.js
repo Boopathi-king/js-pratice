3056130a505ebeef05e4eb443e0bb4b2e9fb07c9
Boopathi-king <boopathiboopathi31@gmail.com>
2024-09-14 22:18:37 +0530

    first commit

---
+++ Assignment 9/demo/src/Components/Navbar.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+import './Navbar.css'
+
+
+const NavBar = () => {
+    
+    return (
+        <div classname="nav">
+        <header className='header'>
+          <a href='/' className='logo'>Logo</a>
+
+          <nav className='navbar'>
+            <a href='/'>Home</a>
+            <a href='/'>About</a>
+            <a href='/'>Portfolio</a>
+            <a href='/'>Services</a>
+            <a href='/'>About us</a>
+          </nav>
+        </header>
+        </div>
+      
+    );
+  };
+  
+  export default NavBar;
